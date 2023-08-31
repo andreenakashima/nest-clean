cceb364d190766dc266e9e9e46fb0575b84585aa
andreenakashima <andreenakashima@gmail.com>
2023-08-31 11:30:05 -0300

    Configurando vitest com swc

---
+++ vitest.config.e2e.ts
@@ -0,0 +1,13 @@
+/// <reference types="vitest" />
+import swc from 'unplugin-swc';
+import { defineConfig } from 'vitest/config';
+import tsConfigPaths from 'vite-tsconfig-paths';
+
+export default defineConfig({
+  test: {
+    include: ['**/*.e2e-spec.ts'],
+    globals: true,
+    root: './src',
+  },
+  plugins: [tsConfigPaths(), swc.vite()],
+});
