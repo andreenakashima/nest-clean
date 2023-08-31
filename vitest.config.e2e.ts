1fdaede65cc2a91e4678d6d8553626e740b32dd4
andreenakashima <andreenakashima@gmail.com>
2023-08-31 16:15:14 -0300

    Banco de dados isolado para os testes

--- vitest.config.e2e.ts
+++ vitest.config.e2e.ts
@@ -8,6 +8,7 @@ export default defineConfig({
     include: ['**/*.e2e-spec.ts'],
     globals: true,
     root: './src',
+    setupFiles: ['../test/setup-e2e.ts'],
   },
   plugins: [tsConfigPaths(), swc.vite()],
 });
