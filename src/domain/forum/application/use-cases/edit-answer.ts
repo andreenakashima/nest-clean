b7fc4f725ef9be70cb8b77d712ed67fe017549cb
andreenakashima <andreenakashima@gmail.com>
2023-09-19 11:15:28 -0300

    Controller: editar resposta

--- src/domain/forum/application/use-cases/edit-answer.ts
+++ src/domain/forum/application/use-cases/edit-answer.ts
@@ -1,70 +1,72 @@
-import { Either, left, right } from "@/core/either";
-import { Answer } from "../../enterprise/entities/answer";
-import { AnswersRepository } from "../repositories/answers-repository";
-import { ResourceNotFoundError } from "@/core/errors/errors/resource-not-found-error";
-import { NotAllowedError } from "@/core/errors/errors/not-allowed-error";
-import { AnswerAttachmentList } from "../../enterprise/entities/answer-attachment-list";
-import { AnswerAttachment } from "../../enterprise/entities/answer-attachment";
-import { UniqueEntityID } from "@/core/entities/unique-entity-id";
-import { AnswerAttachmentsRepository } from "../repositories/answer-attachments-repository";
+import { Either, left, right } from '@/core/either';
+import { Answer } from '../../enterprise/entities/answer';
+import { AnswersRepository } from '../repositories/answers-repository';
+import { ResourceNotFoundError } from '@/core/errors/errors/resource-not-found-error';
+import { NotAllowedError } from '@/core/errors/errors/not-allowed-error';
+import { AnswerAttachmentList } from '../../enterprise/entities/answer-attachment-list';
+import { AnswerAttachment } from '../../enterprise/entities/answer-attachment';
+import { UniqueEntityID } from '@/core/entities/unique-entity-id';
+import { AnswerAttachmentsRepository } from '../repositories/answer-attachments-repository';
+import { Injectable } from '@nestjs/common';
 
 interface EditAnswerUseCaseRequest {
-	authorId: string;
-	answerId: string;
-	content: string;
-	attachmentsIds: string[];
+  authorId: string;
+  answerId: string;
+  content: string;
+  attachmentsIds: string[];
 }
 
 type EditAnswerUseCaseResponse = Either<
-	ResourceNotFoundError | NotAllowedError,
-	{
-		answer: Answer;
-	}
+  ResourceNotFoundError | NotAllowedError,
+  {
+    answer: Answer;
+  }
 >;
 
+@Injectable()
 export class EditAnswerUseCase {
-	constructor(
-		private answersRepository: AnswersRepository,
-		private answerAttachmentsRepository: AnswerAttachmentsRepository
-	) {}
+  constructor(
+    private answersRepository: AnswersRepository,
+    private answerAttachmentsRepository: AnswerAttachmentsRepository,
+  ) {}
 
-	async execute({
-		authorId,
-		answerId,
-		content,
-		attachmentsIds,
-	}: EditAnswerUseCaseRequest): Promise<EditAnswerUseCaseResponse> {
-		const answer = await this.answersRepository.findById(answerId);
+  async execute({
+    authorId,
+    answerId,
+    content,
+    attachmentsIds,
+  }: EditAnswerUseCaseRequest): Promise<EditAnswerUseCaseResponse> {
+    const answer = await this.answersRepository.findById(answerId);
 
-		if (!answer) {
-			return left(new ResourceNotFoundError());
-		}
+    if (!answer) {
+      return left(new ResourceNotFoundError());
+    }
 
-		if (authorId !== answer.authorId.toString()) {
-			return left(new NotAllowedError());
-		}
+    if (authorId !== answer.authorId.toString()) {
+      return left(new NotAllowedError());
+    }
 
-		const currentAnswerAttachment =
-			await this.answerAttachmentsRepository.findManyByAnswerId(answerId);
-		const answerAttachmentList = new AnswerAttachmentList(
-			currentAnswerAttachment
-		);
+    const currentAnswerAttachment =
+      await this.answerAttachmentsRepository.findManyByAnswerId(answerId);
+    const answerAttachmentList = new AnswerAttachmentList(
+      currentAnswerAttachment,
+    );
 
-		const answerAttachments = attachmentsIds.map((attachmentId) => {
-			return AnswerAttachment.create({
-				attachmentId: new UniqueEntityID(attachmentId),
-				answerId: answer.id,
-			});
-		});
+    const answerAttachments = attachmentsIds.map((attachmentId) => {
+      return AnswerAttachment.create({
+        attachmentId: new UniqueEntityID(attachmentId),
+        answerId: answer.id,
+      });
+    });
 
-		answerAttachmentList.update(answerAttachments);
+    answerAttachmentList.update(answerAttachments);
 
-		answer.attachments = answerAttachmentList;
+    answer.attachments = answerAttachmentList;
 
-		answer.content = content;
+    answer.content = content;
 
-		await this.answersRepository.save(answer);
+    await this.answersRepository.save(answer);
 
-		return right({ answer });
-	}
+    return right({ answer });
+  }
 }
