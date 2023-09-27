3b3b7b45519b256ca7c3ef58614078877dddc582
andreenakashima <andreenakashima@gmail.com>
2023-09-27 10:10:06 -0300

    Testando controller de upload

--- src/infra/http/controllers/upload-attachment.controller.e2e-spec.ts
+++ src/infra/http/controllers/upload-attachment.controller.e2e-spec.ts
@@ -37,5 +37,8 @@ describe('Get Question by slug (E2E)', () => {
       .attach('file', './test/e2e/sample-upload.jpg');
 
     expect(response.statusCode).toBe(201);
+    expect(response.body).toEqual({
+      attachmentId: expect.any(String),
+    });
   });
 });
