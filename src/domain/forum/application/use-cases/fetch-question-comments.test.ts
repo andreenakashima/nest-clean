a1b7977a9d430c2fc35ec9fcb478bed38411782c
andreenakashima <andreenakashima@gmail.com>
2023-09-28 10:16:18 -0300

    Listando comentarios com autor

--- src/domain/forum/application/use-cases/fetch-question-comments.test.ts
+++ src/domain/forum/application/use-cases/fetch-question-comments.test.ts
@@ -1,49 +1,89 @@
-import { UniqueEntityID } from "@/core/entities/unique-entity-id";
-import { InMemoryQuestionCommentsRepository } from "test/repositories/in-memory-question-comments-repository";
-import { FetchQuestionCommentsUseCase } from "./fetch-question-comments";
-import { makeQuestionComment } from "test/factories/make-question-comment";
+import { UniqueEntityID } from '@/core/entities/unique-entity-id';
+import { InMemoryQuestionCommentsRepository } from 'test/repositories/in-memory-question-comments-repository';
+import { FetchQuestionCommentsUseCase } from './fetch-question-comments';
+import { makeQuestionComment } from 'test/factories/make-question-comment';
+import { InMemoryStudentsRepository } from 'test/repositories/in-memory-students-repository';
+import { makeStudent } from 'test/factories/make-student';
 
+let inMemoryStudentsRepository: InMemoryStudentsRepository;
 let inMemoryQuestionCommentsRepository: InMemoryQuestionCommentsRepository;
 let sut: FetchQuestionCommentsUseCase;
 
-describe("Fetch question comments", () => {
-	beforeEach(() => {
-		inMemoryQuestionCommentsRepository =
-			new InMemoryQuestionCommentsRepository();
-		sut = new FetchQuestionCommentsUseCase(inMemoryQuestionCommentsRepository);
-	});
+describe('Fetch question comments', () => {
+  beforeEach(() => {
+    inMemoryStudentsRepository = new InMemoryStudentsRepository();
+    inMemoryQuestionCommentsRepository = new InMemoryQuestionCommentsRepository(
+      inMemoryStudentsRepository,
+    );
 
-	it("should be able to fetch question comments", async () => {
-		await inMemoryQuestionCommentsRepository.create(
-			makeQuestionComment({ questionId: new UniqueEntityID("question-1") })
-		);
-		await inMemoryQuestionCommentsRepository.create(
-			makeQuestionComment({ questionId: new UniqueEntityID("question-1") })
-		);
-		await inMemoryQuestionCommentsRepository.create(
-			makeQuestionComment({ questionId: new UniqueEntityID("question-1") })
-		);
+    sut = new FetchQuestionCommentsUseCase(inMemoryQuestionCommentsRepository);
+  });
 
-		const result = await sut.execute({
-			questionId: "question-1",
-			page: 1,
-		});
+  it('should be able to fetch question comments', async () => {
+    const student = makeStudent({ name: 'John Doe' });
 
-		expect(result.value?.questionComments).toHaveLength(3);
-	});
+    inMemoryStudentsRepository.items.push(student);
 
-	it("should be able to fetch paginated question answers", async () => {
-		for (let i = 1; i <= 22; i++) {
-			await inMemoryQuestionCommentsRepository.create(
-				makeQuestionComment({ questionId: new UniqueEntityID("question-1") })
-			);
-		}
+    const comment1 = makeQuestionComment({
+      questionId: new UniqueEntityID('question-1'),
+      authorId: student.id,
+    });
+    const comment2 = makeQuestionComment({
+      questionId: new UniqueEntityID('question-1'),
+      authorId: student.id,
+    });
+    const comment3 = makeQuestionComment({
+      questionId: new UniqueEntityID('question-1'),
+      authorId: student.id,
+    });
 
-		const result = await sut.execute({
-			questionId: "question-1",
-			page: 2,
-		});
+    await inMemoryQuestionCommentsRepository.create(comment1);
+    await inMemoryQuestionCommentsRepository.create(comment2);
+    await inMemoryQuestionCommentsRepository.create(comment3);
 
-		expect(result.value?.questionComments).toHaveLength(2);
-	});
+    const result = await sut.execute({
+      questionId: 'question-1',
+      page: 1,
+    });
+
+    expect(result.value?.comments).toHaveLength(3);
+    expect(result.value?.comments).toEqual(
+      expect.arrayContaining([
+        expect.objectContaining({
+          author: 'John Doe',
+          commentId: comment1.id,
+        }),
+        expect.objectContaining({
+          author: 'John Doe',
+          commentId: comment2.id,
+        }),
+        expect.objectContaining({
+          author: 'John Doe',
+          commentId: comment3.id,
+        }),
+      ]),
+    );
+  });
+
+  it('should be able to fetch paginated question answers', async () => {
+    const student = makeStudent({ name: 'John Doe' });
+
+    inMemoryStudentsRepository.items.push(student);
+
+    for (let i = 1; i <= 22; i++) {
+      await inMemoryQuestionCommentsRepository.create(
+        makeQuestionComment({
+          questionId: new UniqueEntityID('question-1'),
+          authorId: student.id,
+        }),
+      );
+    }
+
+    const result = await sut.execute({
+      questionId: 'question-1',
+      page: 2,
+    });
+
+    expect(result.value?.comments).toHaveLength(2);
+  });
 });
--- test/repositories/in-memory-question-comments-repository.ts
+++ test/repositories/in-memory-question-comments-repository.ts
@@ -1,41 +1,76 @@
-import { PaginationParams } from "@/core/repositories/pagination-params";
-import { QuestionCommentsRepository } from "@/domain/forum/application/repositories/question-comments-repository";
-import { QuestionComment } from "@/domain/forum/enterprise/entities/question-comment";
+import { PaginationParams } from '@/core/repositories/pagination-params';
+import { QuestionCommentsRepository } from '@/domain/forum/application/repositories/question-comments-repository';
+import { QuestionComment } from '@/domain/forum/enterprise/entities/question-comment';
+import { InMemoryStudentsRepository } from './in-memory-students-repository';
+import { CommentWithAuthor } from '@/domain/forum/enterprise/entities/value-objects/comment-with-author';
 
 export class InMemoryQuestionCommentsRepository
-	implements QuestionCommentsRepository
+  implements QuestionCommentsRepository
 {
-	public items: QuestionComment[] = [];
+  public items: QuestionComment[] = [];
 
-	async findById(id: string) {
-		const questionComment = this.items.find(
-			(item) => item.id.toString() === id
-		);
+  constructor(private studentsRepository: InMemoryStudentsRepository) {}
 
-		if (!questionComment) {
-			return null;
-		}
+  async findById(id: string) {
+    const questionComment = this.items.find(
+      (item) => item.id.toString() === id,
+    );
 
-		return questionComment;
-	}
+    if (!questionComment) {
+      return null;
+    }
 
-	async findManyByQuestionId(questionId: string, { page }: PaginationParams) {
-		const questionComments = this.items
-			.filter((item) => item.questionId.toString() === questionId)
-			.slice((page - 1) * 20, page * 20);
+    return questionComment;
+  }
 
-		return questionComments;
-	}
+  async findManyByQuestionId(questionId: string, { page }: PaginationParams) {
+    const questionComments = this.items
+      .filter((item) => item.questionId.toString() === questionId)
+      .slice((page - 1) * 20, page * 20);
 
-	async create(questionComment: QuestionComment) {
-		this.items.push(questionComment);
-	}
+    return questionComments;
+  }
 
-	async delete(questionComment: QuestionComment) {
-		const itemIndex = this.items.findIndex(
-			(item) => item.id === questionComment.id
-		);
+  async findManyByQuestionIdWithAuthor(
+    questionId: string,
+    { page }: PaginationParams,
+  ) {
+    const questionComments = this.items
+      .filter((item) => item.questionId.toString() === questionId)
+      .slice((page - 1) * 20, page * 20)
+      .map((comment) => {
+        const author = this.studentsRepository.items.find((student) => {
+          return student.id.equals(comment.authorId);
+        });
 
-		this.items.splice(itemIndex, 1);
-	}
+        if (!author) {
+          throw new Error(
+            `Author with id "${comment.authorId.toString()}" does not exist`,
+          );
+        }
+
+        return CommentWithAuthor.create({
+          commentId: comment.id,
+          content: comment.content,
+          createdAt: comment.createdAt,
+          updatedAt: comment.updatedAt,
+          authorId: comment.authorId,
+          author: author.name,
+        });
+      });
+
+    return questionComments;
+  }
+
+  async create(questionComment: QuestionComment) {
+    this.items.push(questionComment);
+  }
+
+  async delete(questionComment: QuestionComment) {
+    const itemIndex = this.items.findIndex(
+      (item) => item.id === questionComment.id,
+    );
+
+    this.items.splice(itemIndex, 1);
+  }
 }
